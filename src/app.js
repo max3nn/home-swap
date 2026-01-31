2f4222917a1f4819ef2a1db5da70b8f88f1d7d9c
josh <joshua.97@icloud.com>
2026-01-31 22:18:38 +0000

    Bug fixes and small details

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
 const session = require('express-session');
+const methodOverride = require('method-override');
 const connectDB = require('./config/database');
 const Item = require('./models/Item');
 
@@ -43,6 +44,7 @@ app.use((req, res, next) => {
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Basic route
